d59a07c18c4e6c41108de402ae1ba4aaf4321476
Juan Camilo Cubillos Diaz <juan.cubillos@sofka.com.co>
2020-01-17 14:41:13 -0500

    Added firebase dependecy

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:trips_tolima/platzi_trips.dart';
+import 'package:trips_tolima/user/bloc/block_user.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: PlatziTrips(),
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: PlatziTrips(),
+        ),
+        bloc: UserBloc());
   }
 }
